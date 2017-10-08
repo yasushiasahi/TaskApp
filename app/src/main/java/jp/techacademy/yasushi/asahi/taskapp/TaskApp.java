f5d4530bf0333a500c9dea830db97567d0223f94
yasushiasahi <asahi1600@gmail.com>
2017-10-08 12:39:49 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/yasushi/asahi/taskapp/TaskApp.java
@@ -0,0 +1,13 @@
+package jp.techacademy.yasushi.asahi.taskapp;
+
+import android.app.Application;
+
+import io.realm.Realm;
+
+public class TaskApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+    }
+}
